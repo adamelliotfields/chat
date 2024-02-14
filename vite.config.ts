61e0a36e88dbfadcf119c77140bb695dde3dae3b
Adam Fields <me@aef.me>
2024-02-14 14:38:58 -0500

    Add base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/chat/',
   build: {
     chunkSizeWarningLimit: 5120,
     target: 'esnext'
