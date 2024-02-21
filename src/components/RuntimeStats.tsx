075a611e17b06a6cb685eb6e698cb2a7bd498494
Adam Fields <me@aef.me>
2024-02-21 12:23:53 -0500

    Fix mobile height

--- src/components/RuntimeStats.tsx
+++ src/components/RuntimeStats.tsx
@@ -17,13 +17,13 @@ export function RuntimeStats({ className }: RuntimeStatsProps) {
     <div className={clsx('ml-1 flex items-center md:ml-5', className)}>
       {/* prefilling stats */}
       <div className={classNames}>
-        <span className="text-neutral-900">{stats.prefill} tokens/sec</span>
-        <span className="text-neutral-400">{' prefilling'}</span>
+        <span className="text-neutral-400 block lg:inline">prefilling</span>
+        <span className="text-neutral-900">{` ${stats.prefill}`} tokens/sec</span>
       </div>
       {/* decoding stats */}
-      <div className={clsx('ml-4 md:ml-0', classNames)}>
-        <span className="text-neutral-900">{stats.decode} tokens/sec</span>
-        <span className="text-neutral-400">{' decoding'}</span>
+      <div className={clsx('ml-8 md:ml-4', classNames)}>
+        <span className="text-neutral-400 block lg:inline">decoding</span>
+        <span className="text-neutral-900">{` ${stats.decode}`} tokens/sec</span>
       </div>
     </div>
   )
