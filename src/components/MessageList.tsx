660a8622c2fd7edd231e971111526e322df00211
Adam Fields <me@aef.me>
2024-02-21 12:26:30 -0500

    Improve loading (#6)
    * Disable input when no model loaded (115e780)
    * Disable Shiki (f803f7f)
    * Fix mobile height (075a611)
    * Update README (4da1500)
    * 0.1.2 (962cb82)

--- src/components/MessageList.tsx
+++ src/components/MessageList.tsx
@@ -1,4 +1,4 @@
-import Shiki from '@shikijs/markdown-it'
+// import Shiki from '@shikijs/markdown-it'
 import clsx from 'clsx'
 import { useAtomValue } from 'jotai'
 import MarkdownIt from 'markdown-it'
@@ -8,16 +8,33 @@ import { conversationAtom } from '../atoms'
 import type { Message as IMessage } from '../types'
 
 // can't use `react-markdown` with shiki or rehype-pretty until remarkjs/react-markdown#680
-// (markdown-it is awesome; I'm just depending on `dangerouslySetInnerHTML` to use it)
 const md = MarkdownIt()
-md.use(
-  await Shiki({
-    themes: {
-      light: 'github-light',
-      dark: 'github-dark'
-    }
-  })
-)
+
+// TODO: disabled for performance
+// md.use(
+//   await Shiki({
+//     themes: {
+//       light: 'github-light',
+//       dark: 'github-dark'
+//     }
+//   })
+// )
+
+// external links
+// https://github.com/markdown-it/markdown-it/blob/master/docs/architecture.md#renderer
+const renderToken =
+  md.renderer.rules.link_open ||
+  ((tokens, idx, options, _, self) => self.renderToken(tokens, idx, options))
+
+md.renderer.rules.link_open = (tokens, idx, options, env, self) => {
+  // attrs is a list of [name, value] tuples
+  const href = tokens[idx].attrs?.[0]?.[1]
+  if (href && !href.startsWith('#')) {
+    tokens[idx].attrPush(['target', '_blank'])
+    tokens[idx].attrPush(['rel', 'noopener noreferrer'])
+  }
+  return renderToken(tokens, idx, options, env, self)
+}
 
 export interface MessageProps extends IMessage {
   markdown?: boolean
@@ -34,7 +51,7 @@ export function Message({ content, markdown = false, messageRole }: MessageProps
       >
         {/* message bubble */}
         <div className="max-w-full border px-4 py-2 rounded-lg break-words [word-break:break-word] shadow-sm bg-white">
-          <div className="mt-1 text-xs text-neutral-400 font-bold tracking-tight uppercase">
+          <div className="mt-2 text-xs text-neutral-400 font-bold tracking-tight uppercase">
             {messageRole}
           </div>
           <div
@@ -42,7 +59,7 @@ export function Message({ content, markdown = false, messageRole }: MessageProps
             dangerouslySetInnerHTML={{
               __html: markdown ? md.render(content ?? '') : content ?? ''
             }}
-            className="mt-2 mb-0.5 prose min-w-0"
+            className="mt-4 mb-1 prose min-w-0"
           />
         </div>
       </div>
